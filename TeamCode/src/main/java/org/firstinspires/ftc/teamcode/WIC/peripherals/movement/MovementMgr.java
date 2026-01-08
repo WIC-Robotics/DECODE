2b6230005f50bc615bbec3f978d3d397c43b517b
MohAlhossary <mohammad.alhossary@gmail.com>
2026-01-07 21:12:09 -0500

    work on the WIC.movement package because apparently i did not send all the files
    + starting AutonomousNS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WIC/peripherals/movement/MovementMgr.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WIC/peripherals/movement/MovementMgr.java
@@ -1,10 +1,35 @@
 package org.firstinspires.ftc.teamcode.WIC.peripherals.movement;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class MovementMgr {
 
-    private DrivetrainMgr drivetrain;
-    private DeadwheelMgr deadwheel;
+    private DrivetrainMgr drivetrainMgr;
+    private DeadwheelMgr deadwheelMgr;
 
+    public MovementMgr(HardwareMap hardwareMap) {
+        this.drivetrainMgr = new DrivetrainMgr(hardwareMap);
+        this.deadwheelMgr = new DeadwheelMgr(hardwareMap);
+    }
 
+    public void drive(double translation, double rotation) {
+        //TODO use roadrunner
+        //TODO use Pinpoint Odometry Computer
+        drivetrainMgr.drive(translation, rotation);
 
+    }
+
+    public void stop() {
+        this.drivetrainMgr.stop();
+        this.deadwheelMgr.stop();
+    }
+
+    public void start() {
+        if (drivetrainMgr != null) {
+            this.drivetrainMgr.start();
+        }
+        if (deadwheelMgr != null) {
+            this.deadwheelMgr.start();
+        }
+    }
 }
